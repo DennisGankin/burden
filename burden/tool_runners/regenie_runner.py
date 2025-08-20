ded25cfb236e3d99ccd9e048ede836dafe79f7fb
DennisGankin <dgankin@ethz.ch>
2025-08-20 15:33:15 +0200

    fix

--- burden/tool_runners/regenie_runner.py
+++ burden/tool_runners/regenie_runner.py
@@ -26,7 +26,7 @@ class REGENIERunner(ToolRunner):
         # extend outputs by phenotypes_covariates.formatted.txt
         self._outputs.append(Path('phenotypes_covariates.formatted.txt'))
         # extend by SAMPLES Included
-        self._outputs.append(Path('SAMPLES.Included.txt'))
+        self._outputs.append(Path('SAMPLES_Include.txt'))
         return
 
         # 1. Run step 1 of regenie
